e3c63f3aa50e9fc143d1d7638644ea9a23c2cc91
kirilinsane <kirill.bondyk27@gmail.com>
2020-05-28 14:28:30 +0300

    some buds were fixed

--- app2.js
+++ app2.js
@@ -1,52 +1,49 @@
 'use strict';
 
-console.clear()
-console.log('Enter your city')
+console.clear();
+console.log('Enter your city');
 
 const axios = require('axios');
 
-const getData = async (cityName) => { // functioin for getting data
-	const API_KEY = '3bd68c313a18c02837369e027d9ac4bd';
-	const URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`;
-	const response = await axios.get(URL); // getting request from  API
-	const object = await response.data; // fetting data from request
-	return object;
-}
+const getData = async cityName => { // functioin for getting data
+  const API_KEY = '3bd68c313a18c02837369e027d9ac4bd';
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`;
+  const response = await axios.get(URL); // getting request from  API
+  const object = await response.data; // fetting data from request
+  return object;
+};
 // async anonim function for working with promises
 
 (async () => {
-	let stdin = process.openStdin();
-	const cityName = await new Promise((resolve, reject) => {
-		stdin.addListener("data", d => {
-	 resolve(d.toString().trim());
-	 });
-	});
-	
-	const object = await getData(cityName);;
+  const stdin = process.openStdin();
+  const cityName = await new Promise(resolve => {
+    stdin.addListener('data', d => {
+      resolve(d.toString().trim());
+    });
+  });
 
-	const visibility = 'Visibility is:' + object.visibility + ' ' + 'metres';
-	console.log(visibility);
+  const object = await getData(cityName);
 
-	const sky = 'Sky:' + object.weather.main;
-		console.log(sky);
+  const visibility = 'Visibility is:' + object.visibility  +  'metres';
+  console.log(visibility);
 
-	const temp = 'Current temperature is:' + Math.round(object.main.temp - 273) + '°';
-		console.log(temp);
-		
-	const feelslike = 'Feels like:' + Math.round(object.main.feels_like - 273) + '°';
-		console.log(feelslike);
+  const sky = 'Sky:' + object.weather.main;
+  console.log(sky);
 
-	const minTemp = 'Min temperature for today is:' + Math.round(object.main.temp_min - 273) + '°';
-		console.log(minTemp);
+  const temp = 'Current temperature is:' + Math.round(object.main.temp - 273) + '°';
+  console.log(temp);
 
-	const maxTemp = 'Max temperature for today is:' + Math.round(object.main.temp_max - 273) + '°';
-		console.log(maxTemp);
+  const feelslike = 'Feels like:' + Math.round(object.main.feels_like - 273) + '°';
+  console.log(feelslike);
 
-	const wind = 'Wind speed is:' + object.wind.speed + ' ' + 'm/s';
-		console.log(wind);
+  const minTemp = 'Min temperature for today is:' + Math.round(object.main.temp_min - 273) + '°';
+  console.log(minTemp);
 
-		process.exit(-1)
+  const maxTemp = 'Max temperature for today is:' + Math.round(object.main.temp_max - 273) + '°';
+  console.log(maxTemp);
+
+  const wind = 'Wind speed is:' + object.wind.speed  + 'm/s';
+  console.log(wind);
+
+  process.exit(-1);
 })();
-
-
-
