3e997467b3e2b68210a55048d72e8dbb378ab828
kirilinsane <kirill.bondyk27@gmail.com>
2020-06-16 13:46:24 +0300

    code style fix

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 'use strict';
 
-console.clear();
 const concolor = require('concolor');
 const axios = require('axios');
 
-console.log('Hi');
-console.log(concolor`${'Enter your city'}(b,red)`);
+const greeting = () => {
+  console.clear();
+  console.log('Hi');
+  console.log(concolor`${'Enter your city'}(b,red)`);
+};
+greeting();
 
 const getData = async cityName => {
   const API_KEY = '3bd68c313a18c02837369e027d9ac4bd';
@@ -13,7 +16,7 @@ const getData = async cityName => {
   let objectReq = {};
   await axios.get(URL)
     .then(res => {
-      objectReq  = res.data;
+      objectReq = res.data;
     })
     .catch(err => {
       objectReq = { err };
@@ -21,58 +24,61 @@ const getData = async cityName => {
   return objectReq;
 };
 
+const printInformation = object => {
+  const vis = 'Visibility is';
+  const currentVision = object.visibility;
+  const skyStatus = object.weather.main;
+  if (!currentVision) {
+    console.log(concolor`${'Unknown visibility'}(b,red/black)`);
+  } else {
+    console.log(concolor`${vis}(b,green) ${currentVision + 'm'}(b,yellow)`);
+  }
+  if (!skyStatus) {
+    console.log(concolor`${'No information about the sky'}(b,red/black)`);
+  } else {
+    console.log('sky:' + skyStatus);
+  }
+
+  const getTemp = (gradation = '') => {
+    const value = object.main[gradation];
+    return Math.round(value - 273);
+  };
+
+  const phrase1 = 'Current temperature is:';
+  const currTemperature = getTemp('temp');
+  console.log(concolor`${phrase1}(b,blue) ${currTemperature + '°C'}(b,yellow)`);
+
+  const phrase2 = 'Feels like:';
+  const feelTemperature = getTemp('feels_like');
+  console.log(concolor`${phrase2}(b,blue) ${feelTemperature + '°C'}(b,yellow)`);
+
+  const phrase3 = 'Min temperature for today is:';
+  const minTemperature = getTemp('temp_min');
+  console.log(concolor`${phrase3}(b,blue) ${minTemperature + '°C'}(b,yellow)`);
+
+  const phrase4 = 'Max temperature for today is:';
+  const maxTemperature = getTemp('temp_max');
+  console.log(concolor`${phrase4}(b,blue) ${maxTemperature + '°C'}(b,yellow)`);
+
+  const phrase5 = 'Wind speed is:';
+  const windSpeed = object.wind.speed;
+  console.log(concolor`${phrase5}(b,green) ${windSpeed + 'm/s'}(b,yellow)`);
+
+  process.exit(0);
+};
+
 const input = async () => {
   const stdin = process.openStdin();
-  const cityName = await new Promise(resolve => { //calback
-    stdin.addListener('data', data => { //вытаскиваем дату
+  const cityName = await new Promise(resolve => {
+    stdin.addListener('data', data => {
       resolve(data.toString().trim());
     });
   });
-
   const object = await getData(cityName);
   if (!object.err) {
-    const vis = 'Visibility is';
-    const currentVis =  object.visibility;
-    const skyStatus =  object.weather.main;
-    if (!currentVis) {
-      console.log(concolor`${'Unknown visibility'}(b,red/black)`);
-    } else {
-      console.log(concolor`${vis}(b,green) ${currentVis + 'm'}(b,yellow)`);
-    }
-    if (!skyStatus) {
-      console.log(concolor`${'No information about the sky'}(b,red/black)`);
-    } else {
-      console.log('sky:' + skyStatus);
-    }
-    const getTemp = (gradation = '') => {
-      const value = object.main[gradation];
-      return Math.round(value - 273);
-    };
-
-    const curr = 'Current temperature is:';
-    const tempCurr = getTemp('temp');
-    console.log(concolor`${curr}(b,blue) ${tempCurr + '°C'}(b,yellow)`);
-
-    const feels = 'Feels like:';
-    const tempFeels = getTemp('feels_like');
-    console.log(concolor`${feels}(b,blue) ${tempFeels + '°C'}(b,yellow)`);
-
-    const min = 'Min temperature for today is:';
-    const tempMin = getTemp('temp_min');
-    console.log(concolor`${min}(b,blue) ${tempMin + '°C'}(b,yellow)`);
-
-    const max = 'Max temperature for today is:';
-    const tempMax = getTemp('temp_max');
-    console.log(concolor`${max}(b,blue) ${tempMax + '°C'}(b,yellow)`);
-
-    const wind = 'Wind speed is:';
-    const windSpeed = object.wind.speed;
-    console.log(concolor`${wind}(b,green) ${windSpeed + 'm/s'}(b,yellow)`);
-
-    process.exit(0);
+    printInformation(object);
   } else {
     console.log(concolor`${'Ooops,this city doesn`t exist,try again'}(b,red)`);
-
     input();
   }
 };
